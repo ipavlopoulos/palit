1ef031eca1d5df657922d0bf587a81c0066c29f4
Ioannis Pavlopoulos <annis@aueb.gr>
2023-05-29 16:34:54 +0300

    Update classification.py

--- classification.py
+++ classification.py
@@ -20,58 +20,42 @@ import torch.nn as nn
 import torch.optim as optim
 
 
-
-line_data = pd.read_pickle("palit_lines.csv")
-print("Images num:", line_data.shape[0])
-line_data = line_data.explode("img")
-print("Lines num:", line_data.shape[0])
-
-# removing empty & focusing on 4 centuries
-line_data = line_data[line_data.img.notna()]
-line_data = line_data[line_data.date.isin({1,2,3,4})]
-
-# cleaning
-x_dim = line_data.img.apply(lambda x: x.shape[0]) # mean 50
-y_dim = line_data.img.apply(lambda x: x.shape[1]) # mean 300
-line_data = line_data[(x_dim>50)&(y_dim>300)]
-print("Lines num after preprocessing:", line_data.shape[0])
-
-# resizing
-line_data.img = line_data.img.apply(lambda img: transform.resize(img, (50, 300)))
-
-
-"""# Modeling"""
-print("Modeling...")
-# SPLIT
-#test_ix  = line_data.uid%5==0
-#trainset = line_data[~test_ix]
-#testset = line_data[test_ix]
-#valset, testset = train_test_split(testset, test_size=testset.shape[0]//2, random_state=2023)
-trainset, testset = train_test_split(line_data, test_size=0.1, random_state=39)
-trainset, valset = train_test_split(trainset, test_size=testset.shape[0], random_state=39)
-
-# allowing lines from the same manuscript in the validation
-print("Train, Val, Test:", trainset.shape[0], valset.shape[0], testset.shape[0])
-
-
-class Net(nn.Module):
-    def __init__(self):
+class CNNCLF(nn.Module):
+    def __init__(self, n_classes):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 128, 3, stride=1, padding=0)
-        self.pool = nn.MaxPool2d(3, 3)
-        self.fc1 = nn.Linear(128*1584, 1024, bias=True)
-        self.fc2 = nn.Linear(1024, 128, bias=True)
-        self.dropout = nn.Dropout(0.2)
-        self.fc3 = nn.Linear(128, 4) # regression output
 
+        self.CNN = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=5, stride=1, padding=0),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            nn.BatchNorm2d(32),
+            
+            nn.Conv2d(32, 64, kernel_size=5),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(2, 2),
+        )
+        
+        self.MLP = nn.Sequential(
+            nn.Linear(41472, 1024, bias=True),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm1d(1024),
+            nn.Dropout(p=0.1, inplace=True),
+            
+            nn.Linear(1024, 512, bias=True),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm1d(512),
+            nn.Dropout(p=0.1, inplace=True),
+            
+            nn.Linear(512, n_classes, bias=True)
+        )
+        
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x.unsqueeze(1))))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        #x = self.dropout(x)
-        x = self.fc3(x)
+        x = self.CNN(x.unsqueeze(1))
+        x = torch.flatten(x, 1)
+        x = self.MLP(x)
         return x
+
 
 def validate(model, dataloader, device="cpu"):
     predictions, gold_labels = [], []
@@ -84,30 +68,35 @@ def validate(model, dataloader, device="cpu"):
     return predictions, gold_labels
 
 
-class ImageDataset(TensorDataset):
-    def __init__(self, dataframe, transform=None):
-        self.dataframe = dataframe
-        self.transform = transform
+# the model predicts the average - high variance
+def validate_clf(model, dataloader, device="cpu", criterion = nn.CrossEntropyLoss()):
+    prediction_list, labels_list = [], []
+    model.eval()
+    loss = 0
+    with torch.no_grad():
+        for id_batch, (inputs, labels) in enumerate(dataloader):
+            inputs, labels = inputs.to(device), labels.to(device)
+            model.to(device)
+            outputs = model(inputs)
+            loss += criterion(outputs, labels).item()
+            labels = np.argmax(labels.cpu().detach().numpy(), axis=1)
+            predictions = np.argmax(outputs.cpu().detach().numpy(), axis=1)
+            prediction_list.append(predictions)
+            labels_list.append(labels)
+    p = np.concatenate(prediction_list)
+    g = np.concatenate(labels_list)
+    acc = accuracy_score(g, p)
+    return p, g, loss, acc
 
-    def __len__(self):
-        return self.dataframe.shape[0]
-
-    def __getitem__(self, idx):
-        sample = self.dataframe.iloc[idx].img.astype(np.float32)
-        label = self.dataframe.iloc[idx].date-1 # 0-4 for 1-4CE
-        if self.transform is not None:
-            sample = self.transform(sample)
-            sample = sample.reshape(sample.shape[1], sample.shape[2]) # grayscale
-        return sample, label
-
-def train(model, dataloader, optimizer, criterion, val_dataloader=None, patience=3, val_metric = mean_absolute_error, n_print=10, N_EPOCHS=4, device="cpu"):
+def train_clf(model, dataloader, optimizer, criterion=nn.CrossEntropyLoss(), val_dataloader=None, patience=3, N_EPOCHS=100, device="cpu"):
     losses, mae_losses, val_losses, train_losses = [], [], [], []
     dataset_size = len(dataloader.dataset)
     lowest_error = 100000
     best_epoch = -1
+    
     for epoch in range(N_EPOCHS):
         print(f"Epoch {epoch + 1}\n-------------------------------")
-
+        train_loss = 0
         # Loop over batches in an epoch using DataLoader
         for id_batch, (inputs, labels) in enumerate(dataloader):
             model.train()
@@ -117,68 +106,77 @@ def train(model, dataloader, optimizer, criterion, val_dataloader=None, patience
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-
-            # Print the loss for this batch (every n batches, plus the # examples)
-            if id_batch % n_print == 0:
-                loss, current = loss.item(), (id_batch + 1) * len(inputs)
-                losses.append(loss)
-                mae = nn.L1Loss()
-                mae_loss = mae(outputs.argmax(1).float(), labels)
-                mae_losses.append(mae_loss)
-                print(f"loss: {loss:.2f} (@all: {np.mean(losses):.2f}) mae: {mae_loss:.2f}  [{current:>2d}/{dataset_size:>2d}]")
+            train_loss += loss.item()
 
         if val_dataloader is not None:
-            if (patience>0) and (epoch-best_epoch>patience):
-                print("Max patience reached.")
-                print("Loading the best checkpoint...")
-                model.load_state_dict(torch.load("cnn_clf.pt"))
+            if (patience>0) and (epoch-best_epoch>patience-1) and epoch > 50:
+                print("Max patience reached, now loading the best checkpoint...")
+                model.load_state_dict(torch.load("checkpoint.pt"))
                 print("Exiting...")
                 break
-            predictions, labels = validate(model, val_dataloader, device=device)
-            val_error = val_metric(predictions, labels)
-            train_predictions, train_labels = validate(model, dataloader, device=device)
-            train_error = val_metric(train_predictions, train_labels)
-            val_losses.append(val_error)
-            train_losses.append(train_error)
-            if (patience>0) and (val_error<lowest_error):
-                lowest_error = val_error
+            predictions, labels, val_loss, val_acc = validate_clf(model, val_dataloader, device=device)
+            val_losses.append(val_loss)
+            train_losses.append(train_loss)
+            if (patience>0) and (val_loss<lowest_error):
+                lowest_error = val_loss
                 best_epoch = epoch
-                torch.save(model.state_dict(), "cnn_clf.pt")
-            print(f"Train loss: {train_error:.2f}, Val loss: {val_error:.2f}, Best Epoch: {best_epoch+1} (Val loss: {lowest_error:.2f} - var: {np.var(predictions):.2f})")
-    return losses, mae_losses, train_losses, val_losses
+                torch.save(model.state_dict(), "checkpoint.pt")
+            print(f"Train loss: {train_loss:.2f}, Val loss: {val_loss:.2f}, Val acc: {val_acc:.2f}") 
+            print(f"Best Epoch: {best_epoch+1} (Val loss: {lowest_error:.2f})") 
+            print(f"[val var: {np.var(predictions):.2f}, min-max: {min(predictions):.2f}-{max(predictions):.2f}]")
+    model.eval()
+    return model, train_losses, val_losses
 
+class ImageDataset(TensorDataset):
+    def __init__(self, dataframe, transform=None):
+        self.dataframe = dataframe
+        self.transform = transform
+        self.labels = np.array(self.dataframe['date'])
+        self.labels = np.reshape(self.labels, (self.labels.shape[0], 1))
+        self.labels = OneHotEncoder(sparse=False).fit_transform(self.labels)
+        
+    def __len__(self):
+        return self.dataframe.shape[0]
 
-fragmenting = transforms.Compose([
+    def __getitem__(self, idx):
+        sample, date = self.dataframe.iloc[idx].img.astype(np.float32), self.labels[idx]
+        if self.transform is not None:
+            sample = self.transform(sample)
+            sample = sample.reshape(sample.shape[1], sample.shape[2]) # grayscale
+        return sample, date # 1CE-4CE
+
+augment = transforms.Compose([
     transforms.ToPILImage(),
     transforms.RandomAffine(3), # turn
-    transforms.RandomResizedCrop((50, 300), scale=(0.75, 1.0), ratio=(1,6)), # crop n scale
+    transforms.RandomResizedCrop((50, 300), scale=(0.75, 1.0), ratio=(1, 6)), # crop n scale (add scale to control this)
+    #transforms.RandomCrop((30, 30)),
     transforms.GaussianBlur(3), # blur
     transforms.ToTensor(),
-    transforms.RandomErasing(p=0.5, value=1),
+    transforms.RandomErasing(p=0.5, value=0.5), # erase
 ])
 
-if __name__ == "__main__":
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    net = Net().to(device)
-    mse_loss, mae_loss, train_losses, val_losses = train(net, DataLoader(ImageDataset(trainset, transform=fragmenting), batch_size=32, shuffle=True, drop_last=True),
-                                                         optimizer=optim.Adam(net.parameters(), lr=1e-3), #, weight_decay=1e-5),
-                                                         criterion=nn.CrossEntropyLoss(), # nn.MSELoss()
-                                                         N_EPOCHS=200,
-                                                         device=device,
-                                                         val_metric=mean_absolute_error,
-                                                         patience=40,
-                                                         val_dataloader=DataLoader(ImageDataset(valset), batch_size=1)
-                                                         )
+cc_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.CenterCrop((50, 300)),
+    transforms.ToTensor(),
+])
 
-    print(f"Parameters: {nn.utils.parameters_to_vector(net.parameters()).numel():.1f}")
-    net.eval()
+rs_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize((50,300)),
+    transforms.ToTensor(),
+])
 
-    ev = trainset.sample(100) # evaluating on train
-    predictions, labels = validate(net, DataLoader(ImageDataset(ev), batch_size=1), device=device)
-    print("Train loss:", nn.L1Loss()(torch.Tensor(predictions), torch.Tensor(labels)).numpy())
+white_fragment_ablation = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize((50,300)),
+    transforms.ToTensor(),
+    transforms.RandomErasing(p=1, value=0), # erase
+])
 
-    # evaluation on test
-    predictions, labels = validate(net, DataLoader(ImageDataset(testset), batch_size=1), device=device)
-    print("Test loss", nn.L1Loss()(torch.Tensor(predictions), torch.Tensor(labels)).numpy())
-    testset["cnn"] = predictions
-    testset.to_pickle("clftest.csv")
+black_fragment_ablation = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize((50,300)),
+    transforms.ToTensor(),
+    transforms.RandomErasing(p=1, value=1), # erase
+])
